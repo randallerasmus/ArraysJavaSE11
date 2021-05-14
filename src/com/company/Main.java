3819dd6a2ab34673c4e44075a0a6144e0348d348
randall.erasmus <randallerasmus1@gmail.com>
2021-05-14 23:47:54 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+//What does this code output?
+//
+//
+//        String[] nums = new String[] { "1", "9", "10" };
+//        Arrays.sort(nums);
+//        System.out.println(Arrays.toString(nums));
+//
+//        A. [1, 9, 10]
+//        B. [1, 10, 9]
+//        C. [9, 1, 10]
+//        D. [9, 10, 1]
+//        E. [10, 1, 9]
+//        F. [10, 9, 1]
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
